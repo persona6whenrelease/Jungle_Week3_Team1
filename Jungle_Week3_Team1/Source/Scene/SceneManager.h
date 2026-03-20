65b97be2c8969fc276e2fff5b68611322aabed58
HanYoungSeo <108516412+persona6whenrelease@users.noreply.github.com>
2026-03-20 14:15:17 +0900

    scene manager

--- Jungle_Week3_Team1/Source/Scene/SceneManager.h
+++ Jungle_Week3_Team1/Source/Scene/SceneManager.h
@@ -10,9 +10,18 @@ class SceneManager
 
 public:
 
+	// Factory::ConstructObject 함수로 scene 생성
+
+	//씬을 만들 때 파일을 만들어야함
+
+	// 저장하면 씬의 정보를 읽어서 json을 만들어야함
+
+	// json을 읽어서 씬을 만들거나, 찾아야함
 
 
 
 private:
+
+
 
 };
